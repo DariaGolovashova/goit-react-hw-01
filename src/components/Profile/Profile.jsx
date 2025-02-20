ebc08bd81f9f7b5a917889879790ed6f272d0fd3
Daria Golovashova <your_email>
2025-02-20 13:32:01 +0100

    change list

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,32 +1,25 @@
 import css from "./Profile.module.css"
 
 export default function Profile({ name, tag, location, image, stats }) {  
+  const statsArray = Object.entries(stats);
 return (
-      <div className={css.profile}>
+  <div className={css.profile}>
     <div className={css.description}>
-      
-      <img className={css.avatar} src={image}
+     <img className={css.avatar} src={image}
                 alt="User avatar" width="480" />
       <p className={css.name}> {name}</p>
           <p className={css.tag}>@{tag}</p>
           <p className={css.location}>{location}</p>
           </div>
     
-
-<ul className={css.stats}>
-     
-      <li>
-      <span className={css.label}>Followers</span>
-                <span className={css.quantity}>{stats.followers}</span>
-    </li>
-    <li>
-      <span>Views</span>
-                <span className={css.quantity}>{stats.views}</span>
-    </li>
-    <li>
-      <span>Likes</span>
-                <span className={css.quantity}>{stats.likes}</span>
-    </li>
+    <ul className={css.stats}>
+      {
+        statsArray.map(([key, value]) => ( 
+        <li key={key}>
+          <span className={css.label}>{ key.charAt(0).toUpperCase() + key.slice(1)}</span>
+          <span className={css.quantity}>{value}</span>
+        </li>
+      ))} 
     </ul>
   </div>
   );
